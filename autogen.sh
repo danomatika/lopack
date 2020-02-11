12bc6b31482c5d403560c6fe121e3924190fdb50
danomatika <danomatika@gmail.com>
2020-02-11 13:47:22 +0100

    update autogen for mingw, updated changelog and bump version

--- autogen.sh
+++ autogen.sh
@@ -1,5 +1,20 @@
-#! /bin/bash
+#!/bin/sh
 
-automake --add-missing
-autoreconf -i
+PATH=/sw/bin:$PATH
 
+PWD=${0%/*}
+
+case `uname -s` in
+    MINGW*)
+        # autoreconf doesn't always work on MinGW
+        aclocal --force -I m4 && \
+        libtoolize --install --force && \
+        autoconf --force && \
+        automake --add-missing --copy --force-missing && \
+        true
+    ;;
+    *)
+        automake --add-missing
+        autoreconf --install --force --verbose
+    ;;
+esac
